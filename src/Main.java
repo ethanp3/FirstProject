ae9eb491b363df54f249e022dafee7021f91d3fe
ethanp3 <78411793+ethanp3@users.noreply.github.com>
2022-05-19 14:55:14 +0100

    Data Types

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+
+        long viewsCount = 3_123_456_789L;
+        float price = 10.99F;
+        char letter = 'A';
+        boolean isEligible = false;
+
+        byte age = 30;
+        Date now = new Date();
+        System.out.println(now);
     }
 }
