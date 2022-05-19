a186f52a86175033db00db56b6ced7efadd9c391
ethanp3 <78411793+ethanp3@users.noreply.github.com>
2022-05-19 15:12:21 +0100

    Arrays

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
-import java.util.Date;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isEligible = false;
-
-        byte age = 30;
-        Date now = new Date();
-        System.out.println(now);
+        int[][] numbers = { { 1, 2, 3}, { 4, 5, 6} };
+        System.out.println(Arrays.deepToString(numbers));
     }
 }
